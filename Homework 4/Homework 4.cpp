ee37f6c7f30b79ce0244511c7f1eec151851d06b
Ante <coderasylum@gmail.com>
2023-11-05 03:33:30 +0300

    домашка 4 и 5

---
+++ Homework 4/Homework 4.cpp
@@ -0,0 +1,422 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+
+using namespace std;
+
+
+char sign(int number) {
+    if (number < 0) {
+        return '-';
+    }
+    else if (number == 0) {
+        return '0';
+    }
+    else {
+        return '+';
+    }
+}
+
+bool is_number(string number) {
+    for (int i = 0; i < number.length(); i++)
+    {
+        if ((!isdigit(number.at(i))) * (!(number.at(i) == '-')) * (!(number.at(i) == '+'))) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int rectangle_area(int a, int b) {
+    return (a * b);
+}
+
+int triangle_area(int c, int h) {
+    return (0.5 * c * h);
+}
+
+int circle_area(int r) {
+    return (M_PI * pow(r, 2));
+}
+
+string to_rim_number(int number) {
+    const int values[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    const char* numerals[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+    std::string result = "";
+
+    for (int i = 0; i < sizeof(values) / sizeof(values[0]); ++i) {
+        while (number >= values[i]) {
+            number -= values[i];
+            result += numerals[i];
+        }
+    }
+
+    return result;
+}
+
+int to_arab_number(string s) {
+    unordered_map<char, int> romanMap = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    int result = 0;
+
+    for (int i = 0; i < s.length(); ++i) {
+        if (i > 0 && romanMap[s[i]] > romanMap[s[i - 1]]) {
+            result += romanMap[s[i]] - 2 * romanMap[s[i - 1]];
+        }
+        else {
+            result += romanMap[s[i]];
+        }
+    }
+
+    return result;
+}
+
+int random(int s) {
+    int m = 37, b = 3, c = 64;
+    // int m = 25173, b = 13849, c = 65537;
+    if (s == 0) {
+        return 0;
+    }
+    return ((m * random(s - 1) + b) % c);
+}
+
+const int rows1 = 3;
+const int cols1 = 4;
+const int rows2 = 4;
+const int cols2 = 1;
+
+void dot(float m1[rows1][cols1], float m2[cols1][cols2], float res[rows1][cols2])
+{
+    for (int i = 0; i < rows1; ++i)
+    {
+        for (int j = 0; j < cols2; ++j)
+        {
+            float sum = 0;
+            for (int x = 0; x < cols1; ++x)
+            {
+                sum += m1[i][x] * m2[x][j];
+            }
+            res[i][j] = sum;
+        }
+    }
+}
+
+int findMaxSeller(float sum[rows1][cols2], int size)
+{
+    int mx = 0;
+
+    for (int i = 0; i < size; ++i)
+    {
+        if (sum[mx][0] <= sum[i][0])
+        {
+            mx = i;
+        }
+    }
+    return mx + 1;
+}
+
+int findMinSeller(float sum[rows1][cols2], int size)
+{
+    int mx = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        if (sum[mx][0] >= sum[i][0])
+        {
+            mx = i;
+        }
+    }
+    return mx + 1;
+}
+
+float findSum(float sum[rows1][cols2], int size)
+{
+    float sm = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        sm += sum[i][0];
+    }
+    return sm;
+}
+
+
+string convert(string old_number, int old_base, int new_base) {
+    int number = 0, order;
+    string alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", result = "";
+
+    for (char digit : old_number) {
+        if (alphabet.find(digit) >= old_base) {
+            return "-1";
+        }
+    }
+
+    if (old_base != 10) {
+        order = old_number.size() - 1;
+        for (int i = order; i != -1; i--)
+        {
+            if (isdigit(old_number.at(i))) {
+                int num = (old_number.at(i) - '0') * pow(old_base, order - i);
+                number += num;
+            }
+            else {
+                int num = (old_number.at(i) - '7') * pow(old_base, order - i);
+                number += num;
+            }
+        }
+    }
+    else {
+        number = stoi(old_number);
+    }
+
+    if (new_base == 10) {
+        return to_string(number);
+    }
+    
+    while (number > 0)
+    {
+        result += alphabet.at(number % new_base);
+        number /= new_base;
+    }
+    reverse(result.begin(), result.end());
+    return result;
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int number;
+    cout << "Введите № задания: ";
+    cin >> number;
+    cout << endl;
+    if (number == 1) {
+        string content;
+        ofstream outFile("test.txt");
+
+        if (!outFile.is_open()) {
+            cout << "Не удалось открыть файл" << endl;
+            return 1;
+        }
+
+        cout << "Введите текст для записи в файл (exit для завершения ввода): ";
+        while (getline(cin, content)) {
+            if (content == "exit") {
+                break;
+            }
+            outFile << content << endl;
+        }
+        outFile.close();
+
+        ifstream Opfile("test.txt");
+        if (!Opfile.is_open()) {
+            cout << "Не удалось открыть файл" << endl;
+            return 1;
+        }
+
+        int summa = 0;
+        while (getline(Opfile, content)) {
+            if (content == "") {
+                continue;
+            }
+
+            for (int i = 0; i < content.length(); i++) {
+                if (!isdigit(content.at(i))) {
+                    cout << "В строке должны быть только числа!" << endl;
+                    return 1;
+                }
+                summa += int(content.at(i)) - 48;
+            }
+
+            cout << summa << endl;
+        }
+    }
+    else if (number == 2) {
+        string str_number;
+
+        cout << "Введите число: ";
+        cin >> str_number;
+
+        if (!is_number(str_number)) {
+            cout << "В строке должны быть только числа!" << endl;
+            return 1;
+        }
+
+        int num = stoi(str_number);
+
+        cout << sign(num) << endl;
+    }
+    else if (number == 3) {
+        int a, b, c, h, r;
+        cout << "Введите длинну прямоугольника: " << endl;
+        cin >> a;
+        cout << "Введите ширину прямоугольника: " << endl;
+        cin >> b;
+        cout << "Введите длинну стороны треугольника: " << endl;
+        cin >> c;
+        cout << "Введите высоту треугольника: " << endl;
+        cin >> h;
+        cout << "Введите радиус круга: " << endl;
+        cin >> r;
+
+        if (a > 0 and b > 0) {
+            cout << "Площадь прямоугольника: " << rectangle_area(a, b) << endl;
+        }
+        else {
+            cout << "Площадь прямоугольника: не определена" << endl;
+        }
+        if (c > 0 and h > 0) {
+            cout << "Площадь треугольника: " << triangle_area(c, h) << endl;
+        }
+        else {
+            cout << "Площадь треугольника: не определена" << endl;
+        }
+        if (r > 0) {
+            cout << "Площадь круга: " << circle_area(r) << endl;
+        }
+        else {
+            cout << "Площадь круга: не определена" << endl;
+        }
+
+    }
+    else if (number == 4) {
+        for (int i = 0; i < 13; i++) {
+            for (int j = 0; j < 17; j++) {
+                if (i < 8 and j < 6) {
+                    cout << "*  ";
+                }
+                else {
+                    cout << "---";
+                }
+            }
+            cout << endl;
+        }
+    }
+    else if (number == 5) {
+        const int width = 120;
+        const int height = 30;
+
+        const float amplitude = 10.0f;
+        const float frequency = 0.1f;
+
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                float sineValue = amplitude * sin(frequency * x);
+                int pixelY = static_cast<int>(sineValue) + height / 2;
+
+                if (y == pixelY) {
+                    cout << "*";
+                }
+                else {
+                    cout << " ";
+                }
+                    
+            }
+            cout << '\n';
+        }
+
+        return 0;
+    }
+    else if (number == 6) {
+        string num;
+        cout << "Введите римское число: ";
+        cin >> num;
+        
+        int arab_number = to_arab_number(num);
+        if (num != to_rim_number(arab_number)) {
+            cout << "Неправильное римское число!";
+        }
+        else {
+            cout << arab_number;
+        }
+    }
+    else if (number == 7) {
+        cout << random(1) << endl;
+        cout << random(2) << endl;
+        cout << random(3) << endl;
+        cout << random(4) << endl;
+    }
+    else if (number == 8) {
+        float sales[rows1][cols1] = {
+        {5, 2, 0, 10},
+        {3, 5, 2, 5},
+        {20, 0, 0, 0} };
+
+        float costs[cols1][cols2] = {
+            {1.2},
+            {2.8},
+            {5.0},
+            {2.0} };
+
+        float comission[cols1][cols2] = {
+            {0.5},
+            {0.4},
+            {1.0},
+            {1.5} };
+
+        float sumSales[rows1][cols2];
+        float sumComission[rows1][cols2];
+
+        dot(sales, costs, sumSales);
+        dot(sales, comission, sumComission);
+
+        cout << "Max sales made seller: " << findMaxSeller(sumSales, rows1) << endl;
+        cout << "Min sales made seller: " << findMinSeller(sumSales, rows1) << endl;
+
+        cout << "Max comiss. made seller: " << findMaxSeller(sumComission, rows1) << endl;
+        cout << "Min comiss. made seller: " << findMinSeller(sumComission, rows1) << endl;
+
+        cout << "Sum: " << findSum(sumSales, rows1) - findSum(sumComission, rows1) << endl;
+        cout << "Sum of comiss: " << findSum(sumComission, rows1) << endl;
+        cout << "Sum through sales: " << findSum(sumSales, rows1) << endl;
+
+        return 0;
+    }
+    else if (number == 9) {
+        int old_base = 0, new_base = 0;
+        string old_number, new_number, sold_base, snew_base;
+
+        cout << "Введите число, которое необходимо перевести: ";
+        cin >> old_number;
+        cout << "Введите систему счисления числа, которое нужно перевести: ";
+        cin >> sold_base;
+        cout << "Введите систему счисления, в которую вы хотите перевести число: ";
+        cin >> snew_base;
+
+        if (!(is_number(sold_base)) or !(is_number(snew_base))) {
+            cout << "Неправильный формат ввода данных" << endl;
+            return 1;
+        }
+
+        old_base = stoi(sold_base);
+        new_base = stoi(snew_base);
+
+        if ((old_base > 36) or (old_base < 2) or (new_base > 36) or (new_base < 2)) {
+            cout << "Система счисления должна быть в диапозоне от 2 до 36!" << endl;
+            return 1;
+        }
+
+        new_number = convert(old_number, old_base, new_base);
+        if (new_number == "-1") {
+            cout << "Неправильное число!" << endl;
+        }
+        else {
+            cout << new_number << endl;
+        }
+    }
+}
---
+++ Homework 5/Homework 5.cpp
@@ -0,0 +1,573 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <random>
+#include <cstdlib>
+
+using namespace std;
+
+double log_base(double number, double base) {
+    return log(number) / log(base);
+}
+
+bool is_number(string number) {
+    for (char c : number) {
+        if (!isdigit(c) and c != '-' and c != ',') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+bool is_trivial_number(int number) {
+    int number_sqrt = sqrt(number);
+    for (int i = 2; i < number_sqrt + 1; i++) {
+        if (number % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+string convert(double N, int base) {
+    string result = "";
+    int tipon = N;
+    double tfpon = modf(N, &N);
+    while (tipon > 0) {
+        result = to_string(tipon % base) + result;
+        tipon /= base;
+    }
+
+    result += ".";
+
+    int m;
+    for (int i = 1; i <= 6; i++) {
+        if (tfpon * pow(10, i) == (int) (tfpon * pow(10, i))) {
+            m = -i;
+            break;
+        }
+        else {
+            m = -4;
+        }
+    }
+
+    int L = -log_base(pow(10, m) / 2, base);
+    for (int i = 0; i < L; i++) {
+        tfpon *= base;
+        result = result + to_string((int)tfpon);
+        tfpon = modf(tfpon, &tfpon);
+    }
+    return result;
+}
+
+int task_1_1(int a, int b) {
+    int number_1, number_2, new_remain, remain;
+
+    if (a == 0 and b == 0) {
+        return -1;
+    }
+    else if (a == 0 or b == 0){
+        return a + b;
+    }
+
+    if (a > b) {
+        number_1 = a; number_2 = b;
+    }
+    else {
+        number_1 = b; number_2 = a;
+    }
+
+    remain = number_1 % number_2;
+    if (remain == 0) {
+        return number_2;
+    }
+
+    while (number_2 % remain != 0 and number_2 > 0) {
+        new_remain = number_2 % remain;
+        number_2 = remain;
+        remain = new_remain;
+    }
+    return remain;
+}
+
+int task_1_2(int a, int b) {
+    if (a == 0 and b == 0) {
+        return -1;
+    }
+    else if (a == 0 or b == 0) {
+        return a + b;
+    }
+
+    while (a > 0 and b > 0) {
+        if (a > b) {
+            a -= b;
+        }
+        else {
+            b -= a;
+        }
+    }
+    return a + b;
+}
+
+
+string task_2(int number) {
+    string result = "";
+    for (int i = 2; i <= number; i++) {
+        if (is_trivial_number(i)) {
+            result += to_string(i) + " ";
+        }
+    }
+    return result;
+}
+
+int* task_3() {
+    string content, word;
+    ofstream new_file("task_3.txt");
+
+    cout << "Введите текст для записи в файл (<exit> для завершения ввода): " << endl;
+    while (getline(cin, content)) {
+        if (content == "exit") {
+            break;
+        }
+        new_file << content;
+    }
+    new_file.close();
+
+    cout << "Введите слово, которое хотите найти в текстовом файле: ";
+    cin >> word;
+
+    ifstream file("task_3.txt");
+    int line = 0, index;
+    while (getline(file, content)) {
+        line++;
+        index = content.find(word);
+        if (index != -1) {
+            int* answer = new int[2];
+            answer[0] = index; answer[1] = line;
+            return answer;
+        }
+    }
+    int* answer = new int[1];
+    answer[0] = -1;
+    return answer;
+}
+
+int task_4(string text, string under_string) {
+    return text.find(under_string);
+}
+
+double task_5(int x) {
+    double sum = 0;
+    for (int i = 0; i <= 10; i++) {
+        sum += ((tgamma(2 * i + 2) / (pow(i + 1, i) * pow(x, 4 * i))));
+    }
+    return sum;
+}
+
+void task_6() {
+    string input = " ";
+    int k = 0;
+    double x, sum = 0, ar = 0;
+    cin >> input;
+    while (input != "exit") {
+        if (!is_number(input)) {
+            cout << "Неправильное число!" << endl;
+            continue;
+        }
+        x = stod(input);
+        sum += x;
+        k++;
+        ar = sum / k;
+        cout << "Введено чисел: " << k << " Сумма: " << sum << " Сред. арифметическое: " << ar << endl;
+        cin >> input;
+    } 
+}
+
+void task_7(int N, int base) {
+    double number;
+    string content;
+    ofstream new_file("task_7_before.txt");
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<double> distribution(0.0, 10.0);
+
+    new_file.imbue(locale(""));
+    
+    for (int i = 0; i < N; i++) {
+        number = distribution(gen);
+        new_file << fixed << number << endl;
+    }
+    new_file.close();
+
+    int i = 0;
+    string* numbers = new string[N];
+    ifstream file("task_7_before.txt");
+    while (getline(file, content)) {
+        number = stod(content);
+        numbers[i] = convert(number, base);
+        i++;
+    }
+    file.close();
+
+    ofstream finish_file("task_7_after.txt");
+    for (int i = 0; i < N; i++) {
+        finish_file << numbers[i] << endl;
+    }
+    new_file.close();
+
+    delete[] numbers;
+}
+
+void task_8() {
+    // Одномерные массивы #33
+
+    // Создание одномерного массива и заполнение его элементами
+    int* A = new int[10];
+    cout << "Изначальный массив: ";
+    for (int i = 0; i < 10; i++) {
+        A[i] = rand() % 100 + 1;
+        cout << to_string(A[i]) + " ";
+    }
+    cout << endl;
+
+    // Поиск необходимого элемента
+    for (int i = 1; i < 9; i++) {
+        if (A[0] < A[i] and A[i] < A[9]) {
+            cout << "Номер элемента: " << i + 1 << endl;
+            break;
+        }
+    }
+
+    // Освобождение памяти
+    delete[] A;
+}
+
+void task_9(int N) {
+    // Задачи сортировки #43
+
+    // Создание одномерного массива и заполнение его элементами
+    int* A = new int[N];
+    cout << "Изначальный массив: ";
+    for (int i = 0; i < N; i++) {
+        A[i] = rand() % 100 + 1;
+        cout << to_string(A[i]) + " ";
+    }
+
+    // "Переворачивание" массива
+    int temp = 0;
+    for (int i = 0; i < N / 2; i++) {
+        temp = A[N - 1 - i];
+        A[N - 1 - i] = A[i];
+        A[i] = temp;
+    }
+
+    // Вывод обновлённого массива
+    cout << endl << "Обновлённый массив: ";
+    for (int i = 0; i < N; i++) {
+        cout << to_string(A[i]) + " ";
+    }
+
+    // Освобождение памяти
+    delete[] A;
+}
+
+void task_10(int N) {
+    // Двумерные массивы #56
+    // Создание массива для хранения данных чисел (в данном случае рандомных)
+    int* a = new int[N];
+    cout << "Данные числа: ";
+    for (int i = 0; i < N; i++) {
+        a[i] = rand() % 100 + 1;
+        cout << to_string(a[i]) + " ";
+    }
+
+    // Создание двумерного массива (т.е. матрицы)
+    int** b = new int* [N];
+    for (int i = 0; i < N; ++i) {
+        b[i] = new int[N];
+    }
+
+    // Заполнение матрицы по заданной формуле
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            b[i][j] = a[i] - 3 * a[j];
+        }
+    }
+
+    // Вывод матрицы
+    cout << endl;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Освобождение памяти
+    for (int i = 0; i < N; ++i) {
+        delete[] b[i];
+    }
+    delete[] a, b;
+}
+
+void print_matrix(int** matrix, int rows, int cols) {
+    cout << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void task_11() {
+    int M = 2, N = 5;
+
+    // Создание матриц
+    int** A = new int* [N];
+    int** B = new int* [N];
+    int** C = new int* [N];
+    int** D = new int* [N];
+    int** E = new int* [N];
+    for (int i = 0; i < M; ++i) {
+        A[i] = new int[N];
+        B[i] = new int[N];
+        C[i] = new int[N];
+        D[i] = new int[N];
+        E[i] = new int[N];
+    }
+
+    // Заполнение матрицы A
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            int input;
+            cin >> input;
+            A[i][j] = input;
+        }
+    }
+
+    // Заполнение матрицы B (по формуле)
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            B[i][j] = i * j - (5 - i - j);
+        }
+    }
+
+    // Заполнение матрицы С (случайные числа на отрезке [-3; 6])
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = rand() % (6 - (-3) + 1) + (-3);
+        }
+    }
+
+    // Заполнение матрицы E (единицы)
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            E[i][j] = 1;
+        }
+    }
+
+    // Заполнение матрицы D (D=6E-C-2B-3A)
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            D[i][j] = 6 - C[i][j] - 2 * B[i][j] - 3 * A[i][j];
+        }
+    }
+
+    // Вывод матрицы A
+    cout << endl << "Матрица A:";
+    print_matrix(A, M, N);
+    cout << endl << "Матрица B:";
+    print_matrix(B, M, N);
+    cout << endl << "Матрица C:";
+    print_matrix(C, M, N);
+    cout << endl << "Матрица E:";
+    print_matrix(E, M, N);
+    cout << endl << "Матрица D:";
+    print_matrix(D, M, N);
+
+    // Освобождение памяти
+    for (int i = 0; i < M; ++i) {
+        delete[] A[i];
+        delete[] B[i];
+        delete[] C[i];
+        delete[] D[i];
+        delete[] E[i];
+    }
+    delete[] A, B, C, D, E;
+}
+
+void task_12(int N) {
+    // Переборные задачи #88
+
+    // Создание массива и заполнение его значениеями (в данном случае рандомными)
+    int* sequence = new int[N];
+    cout << "Данная последовательность: ";
+    for (int i = 0; i < N; i++) {
+        sequence[i] = rand() % 10 + 1;
+        cout << to_string(sequence[i]) + " ";
+    }
+
+    // Вычисление самой длинной возрастающей последовательности
+    cout << endl;
+    int count = 1, max_count = 1;
+    string subsequence, max_subsequence;
+    subsequence = to_string(sequence[0]) + " ";
+    for (int i = 0; i < N - 1; i++) {
+        if (sequence[i] < sequence[i+1]) {
+            count += 1;
+            subsequence += to_string(sequence[i + 1]) + " ";
+        }
+        else {
+            if (count > max_count) {
+                max_count = count;
+                max_subsequence = subsequence;
+            }
+            count = 1;
+            subsequence = to_string(sequence[i + 1]) + " ";
+        }
+    }
+    cout << "Максимальная длинна возрастающей последовательности: " << max_count << endl;
+    cout << "Последовательность: " << max_subsequence << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int number;
+    cout << "Введите № задания: ";
+    cin >> number;
+    cout << endl;
+    if (number == 1) {
+        string input_1, input_2;
+        cout << "Введите первое число: ";
+        cin >> input_1;
+        cout << "Введите второе число: ";
+        cin >> input_2;
+        if (!is_number(input_1) or !is_number(input_2)) {
+            cout << "Неправильный ввод!";
+            return 0;
+        }
+        cout << "Наибольший общий делитель (делениями): " << task_1_1(stoi(input_1), stoi(input_2)) << endl;
+        cout << "Наибольший общий делитель (вычитаниями): " << task_1_2(stoi(input_1), stoi(input_2)) << endl;
+    }
+    else if (number == 2) {
+        string input;
+        cout << "Введите число: ";
+        cin >> input;
+        if (!is_number(input)) {
+            cout << "Неправильный ввод!";
+            return 0;
+        }
+        cout << "Простые числа: " << task_2(stoi(input)) << endl;
+    }
+    else if (number == 3) {
+        int* index = task_3();
+        if (index[0] != -1) {
+            cout << "Слово найдено, линия: " << index[1] << ", позиция первого символа: " << index[0] << endl;
+        }
+        else {
+            cout << "Слово не найдено" << endl;
+        }
+
+        delete[] index;
+    }
+    else if (number == 4) {
+        string text, under_string;
+        cout << "Введите строку: ";
+        cin >> text;
+        cout << "Введите подстроку: ";
+        cin >> under_string;
+        int index = task_4(text, under_string);
+        if (index != -1) {
+            cout << "Подстрока найдена, позиция первого символа: " << index << endl;
+        }
+        else {
+            cout << "Подстрока не найдена" << endl;
+        }
+    }
+    else if (number == 5) {
+        string input;
+        cout << "Введите x: ";
+        cin >> input;
+        if (!is_number(input)) {
+            cout << "Неправильный ввод!";
+            return 0;
+        }
+        int x = stoi(input);
+        cout << fixed << setprecision(6);
+        cout << "Сумма: " << task_5(x) << endl;
+    }
+    else if (number == 6) {
+        cout << "Обработка последовательности дробных чисел. После ввода каждого числа нажимайте <Enter>" << endl;
+        task_6();
+    }
+    else if (number == 7) {
+        string input_1, input_2;
+        cout << "Введите число N: ";
+        cin >> input_1;
+        cout << "Введите систему счисления: ";
+        cin >> input_2;
+        if (!is_number(input_1) or !is_number(input_2)) {
+            cout << "Неправильный ввод!";
+            return 0;
+        }
+        int N = stoi(input_1);
+        int base = stoi(input_2);
+
+        if (2 > base or base > 9) {
+            cout << "Система счисления должна быть в пределах от двочиной до девятеричной" << endl;
+            return 0;
+        }
+        task_7(N, base);
+    }
+    else if (number == 8) {
+        // Одномерные массивы #33
+        task_8();
+    }
+    else if (number == 9) {
+        // Задачи сортировки #43
+        string input;
+        cout << "Введите N: ";
+        cin >> input;
+        if (!is_number(input)) {
+            cout << "Неправильный ввод!" << endl;
+            return 0;
+        }
+        int N = stoi(input);
+        task_9(N);
+    }
+    else if (number == 10) {
+        // Двумерные массивы #56
+        string input;
+        cout << "Введите N: ";
+        cin >> input;
+        if (!is_number(input)) {
+            cout << "Неправильный ввод!" << endl;
+            return 0;
+        }
+        int N = stoi(input);
+        task_10(N);
+    }
+    else if (number == 11) {
+        // Функции #66
+        task_11();
+    }
+    else if (number == 12) {
+        // Переборные задачи #88
+        string input;
+        cout << "Введите N: ";
+        cin >> input;
+        if (!is_number(input)) {
+            cout << "Неправильный ввод!" << endl;
+            return 0;
+        }
+        int N = stoi(input);
+        task_12(N);
+    }
+    cout << endl;
+    main();
+}
